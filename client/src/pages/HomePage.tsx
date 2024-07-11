5a975b99896f3b43af35fba284e8c70286eeb913
A. J <adanj270594@gmail.com>
2024-07-10 21:27:04 -0400

    fix: build

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { HomeCarousel } from '@/components/HomeCarousel';
 import { Cards } from '@/components/HomeCards';
 import {
