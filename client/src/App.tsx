61936020cb6ac4f06d94447af5ebb6c62714934d
Teresa <mtmaisterra@gmail.com>
2024-07-12 19:46:05 -0300

    vista Tarea Rechazada

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
+import TareaRechazada from './pages/TareaRechazada';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='*' element={<NotFound />} />
+      <Route path='/TareaRechazada' element={<TareaRechazada />} />
     </Routes>
   );
 }
