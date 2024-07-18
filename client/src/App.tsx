275b9fdf17f97fb50b886be34b4d38d816482c60
Teresa <mtmaisterra@gmail.com>
2024-07-18 20:51:56 -0300

    Vista Propuesta Detallada

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from './pages/NotFound';
 import TareaRechazada from './pages/TareaRechazada';
 import NuevaTarea from './pages/NuevaTarea';
 import TareaConfirmada from './pages/TareaConfirmada';
+import PropuestaDetallada from './pages/PropuestaDetallada';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route path='/TareaRechazada' element={<TareaRechazada />} />
       <Route path='/nuevaTarea' element={<NuevaTarea />} />
       <Route path='/TareaConfirmada' element={<TareaConfirmada />} />
+      <Route path='/PropuestaDetallada' element={<PropuestaDetallada />} />
     </Routes>
   );
 }
