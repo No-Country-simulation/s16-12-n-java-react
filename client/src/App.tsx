40f04cfdcf9ce97f8653953917e5dac17533fa38
Teresa <mtmaisterra@gmail.com>
2024-07-18 20:59:41 -0300

    Vista Revisión de tareas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from './pages/NotFound';
 import TareaRechazada from './pages/TareaRechazada';
 import NuevaTarea from './pages/NuevaTarea';
 import TareaConfirmada from './pages/TareaConfirmada';
+import RevisionDeTareas from './pages/RevisionDeTareas';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route path='/TareaRechazada' element={<TareaRechazada />} />
       <Route path='/nuevaTarea' element={<NuevaTarea />} />
       <Route path='/TareaConfirmada' element={<TareaConfirmada />} />
+      <Route path='/RevisionDeTareas' element={<RevisionDeTareas />} />
     </Routes>
   );
 }
